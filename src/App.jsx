b3d2a4b444392cd57881ed41697f1da5e40e3614
Umunna Joseph <umunnajoseph70@gmail.com>
2024-01-05 11:43:21 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,48 @@
 import styles from "./style";
-import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+import Billing from "./components/Billing.jsx";
+import Business from "./components/Business";
+import Button from "./components/Button";
+import CardDeal from "./components/CardDeal";
+import Clients from "./components/Clients";
+import CTA from "./components/CTA";
+import FeedbackCard from "./components/FeedbackCard";
+import Footer from "./components/Footer";
+import GetStarted from "./components/GetStarted";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Stats from "./components/Stats";
+import Testimonials from "./components/Testimonials";
+
+
 
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
+  
+  <div className="bg-primary w-full overflow-hidden ">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        <Navbar />
-      </div>
+       <Navbar />
+        </div>
     </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Hero />
-      </div>
+    <div className={`bg-primary  ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Hero/>
     </div>
-    
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
-        <Stats />
-        <Business />
-        <Billing />
-        <CardDeal />
-        <Testimonials />
-        <Clients />
-        <CTA />
-        <Footer />
-      </div>
+  </div>
+
+  <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Stats/>
+      <Business/>
+      <Billing/>
+      <CardDeal/>
+      <Testimonials/>
+      <Clients/>
+      <CTA/>
+      <Footer/>
+
     </div>
+  </div>
   </div>
 );
 
